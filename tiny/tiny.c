8602bb3cf364a0ebb1a426199eb33abb0e1a6cd9
ssongwon <song42782@gmail.com>
2023-09-17 13:27:37 +0000

    연습문제 완

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -12,7 +12,7 @@
 void doit(int fd);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
+void serve_static(int fd, char *filename, int filesize, int isHEAD);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
@@ -29,33 +29,69 @@ int main(int argc, char **argv) {
   }
 
   listenfd = Open_listenfd(argv[1]); // 포트번호 넣으면 파일디스크립터 정수 받아옴
+
   while (1) {
     clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept
+
+    // 코드는 여기서 블락됨 sys.socket.h에 accept 정의되어 있음
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+    printf("커넥션 연결 : %d\n", connfd);
+
+    // 버퍼에 호스트이름이랑 port 값 인자로 넘겨준 버퍼에 저장됨
+    //int getnameinfo(const struct sockaddr* sa, socklen_t salen, char* host, socklen_t hostlen, char* serv, socklen_t servlen, int flags);
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    // 잘 저장됐는지 확인
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
+
+    // 뭔가 함
+    doit(connfd);  
+
+    //그리고 파일디스크립터 닫아줌 (다 쓰고 안닫으면 FD 누수 발생)
+    Close(connfd); 
   }
 }
 
+// 한번의 요청-응답만 가능. 그다음은 연결 종료됨. 새로고침할때마다 새로운 커넥션 생기는 거 확인 가능
 void doit(int fd) {
+    printf("do it 함수 시작\n");
   int is_static;
   struct stat sbuf;
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   char filename[MAXLINE], cgiargs[MAXLINE];
   rio_t rio;
 
+  // read 버퍼 초기화
   Rio_readinitb(&rio, fd);
+
+  // read() 함수에서 값 들어올때까지 블로킹 
   Rio_readlineb(&rio, buf, MAXLINE);
+  
+  // 보통 이런 값 들어와 개발자도구에서 네트워크 탭 헤더에 적혀있어
+  /*
+  GET / HTTP/1.1
+  Accept: text/html,application/xhtml+xml
+  Accept-Encoding: gzip, deflate
+  Accept-Language: ko-KR,ko;q=0.9
+  Connection: keep-alive
+  Host: 3.36.130.238:8080
+  Upgrade-Insecure-Requests: 1
+  User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.2 Safari/605.1.15
+  */
+
   printf("Request headrs :\n");
   printf("%s", buf);
   sscanf(buf, "%s %s %s", method, uri, version);
-  if (strcasecmp(method, "GET")) {
+  if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) {
     clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
     return;
   }
   read_requesthdrs(&rio);
+  int isHEAD = 0;
+  if (strcmp(method, "HEAD")==0 || strcmp(method, "head")==0)  {
+    isHEAD = 1;
+  }
+  printf("isHEAD = %d\n", isHEAD);
 
   is_static = parse_uri(uri, filename, cgiargs);
   if (stat(filename, &sbuf) < 0) {
@@ -68,7 +104,8 @@ void doit(int fd) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
       return;
     }
-    serve_static(fd, filename, sbuf.st_size);
+    printf("===파일 이름=== : %s\n", filename);
+    serve_static(fd, filename, sbuf.st_size, isHEAD);
   } else {
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
@@ -76,6 +113,8 @@ void doit(int fd) {
     }
     serve_dynamic(fd, filename, cgiargs);
   }
+  
+  printf("do it 함수 종료\n");
 }
 
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
@@ -132,7 +171,7 @@ int parse_uri(char *uri, char *filename, char *cgiargs) {
   }
 }
 
-void serve_static(int fd, char *filename, int filesize) {
+void serve_static(int fd, char *filename, int filesize, int isHEAD) {
   int srcfd;
   char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
@@ -143,14 +182,26 @@ void serve_static(int fd, char *filename, int filesize) {
   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
   sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
   Rio_writen(fd, buf, strlen(buf));
-  printf("Response headers:\n");
+  //printf("Response headers:\n");
   printf("%s", buf);
 
-  srcfd = Open(filename, O_RDONLY, 0);
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-  Close(srcfd);
-  Rio_writen(fd, srcp, filesize);
-  Munmap(srcp, filesize);
+  if (isHEAD!= 1) {
+    srcfd = Open(filename, O_RDONLY, 0);
+    // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    srcp = (char *) malloc(filesize);
+    int rc;
+
+    if ((rio_readn(srcfd, srcp, filesize)) < 0)
+      unix_error("Rio_readnb error");
+
+    // Rio_writen(fd, srcp, filesize);
+    if (rio_writen(fd, srcp, filesize) != filesize)
+      unix_error("Rio_writen error");
+    Close(srcfd);  
+    // Munmap(srcp, filesize);
+    free(srcp);
+  }
+
 }
 
 void get_filetype(char *filename, char *filetype) {
@@ -162,11 +213,16 @@ void get_filetype(char *filename, char *filetype) {
     strcpy(filetype, "image/png");
   else if (strstr(filename, ".jpg"))
     strcpy(filetype, "image/jpeg");
+  else if (strstr(filename, ".mp4")) 
+    strcpy(filetype, "video/mp4");
   else
     strcpy(filetype, "text/plain");
 }
 
 void serve_dynamic(int fd, char *filename, char *cgiargs) {
+  printf("server_dynamic 실행\n");
+
+  printf("filename = %s\n", filename);
   char buf[MAXLINE], *emptylist[] = { NULL };
   sprintf(buf, "HTTP/1.0 200 OK\r\n");
   Rio_writen(fd, buf, strlen(buf));
@@ -179,3 +235,4 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
   }
   Wait(NULL); 
 }
+
